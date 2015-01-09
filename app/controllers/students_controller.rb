8ea9d7078deddf81a83553f5ccecbc95298da54c
James Corey <mmjames04@yahoo.com>
2015-01-08 19:28:00 -0500

    students almost done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,10 +2,12 @@ class StudentsController < ApplicationController
 
   def index
   	@students = Student.all
+  	# @house = House.where(:id => Student.house_id)
   end
 
   def show
-    @student = Student.find(:id)
+    @student = Student.find(params[:id])
+    @house = House.find_by(:id => @student.house_id)
   end
 
 end
