6a2cca61909ed86fd51f5dc9b0289c6c7aaa1265
James Corey <mmjames04@yahoo.com>
2015-01-08 19:03:44 -0500

    houses done

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,11 +1,12 @@
 class HousesController < ApplicationController
 
   def index
-    @houses = Houses.all
+    @houses = House.all
   end
 
   def show
-    @houses = Houses.all
+    @house = House.find(params[:id])
+    @students = Student.all
   end
 
 end
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,4 @@
 class House < ActiveRecord::Base
+	has_many :students
+
 end
