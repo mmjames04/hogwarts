4464ba8d760aff9789c2f7a9c5803e5cef395b60
James Corey <mmjames04@yahoo.com>
2015-01-08 20:21:10 -0500

    done for the night

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,4 +10,18 @@ class StudentsController < ApplicationController
     @house = House.find_by(:id => @student.house_id)
   end
 
+  def create
+  	@new_student = Student.create(student_params)
+  	@new_student_house = 1..4.sample
+  end
+
+  def new
+  	@new_student = Student.new
+  end
+
+  private
+
+  	def student_params
+      params.require(:student).permit(:name, :img_url, :house_id)
+    end
 end
